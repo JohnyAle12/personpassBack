04f809e57860f387da17d898946eaf8a5a32bd60
Johny Prieto <johnyprieto001@gmail.com>
2023-01-21 08:31:54 -0500

    implements queue system to dispatch events

--- app/Listeners/CreateUserAccount.php
+++ app/Listeners/CreateUserAccount.php
@@ -1,13 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Listeners;
 
 use App\Events\UserProcessed;
 use App\Services\AccountService;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 
-class CreateUserAccount
+class CreateUserAccount implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -25,7 +26,7 @@ class CreateUserAccount
      * @param  \App\Events\UserProcessed  $event
      * @return void
      */
-    public function handle(UserProcessed $event)
+    public function handle(UserProcessed $event): void
     {
         $this->accountService->store($event->user);
     }
